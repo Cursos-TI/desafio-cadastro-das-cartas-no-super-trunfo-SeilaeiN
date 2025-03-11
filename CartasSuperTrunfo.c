2f6124ce4335517aa10e921115d154575c78bc00
Henrique Santos <rikowow89@gmail.com>
2025-03-11 02:05:52 -0300

    CartasSuperTrunfo.c
    Ajuste na variavel populacao1 e populacao2 de int para float

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,23 +3,23 @@
 #include <stdio.h>
 
 int main() {
-    char estado1, estado2;
+    char estado1[3], estado2[3];
     char codigo1[4], codigo2[4];
     char nomeCidade1[20], nomeCidade2[20]; // Sem espaços
-    int populacao1, populacao2;
+    float populacao1, populacao2;
     float area1, area2;
     float pib1, pib2;
     int pontosTuristicos1, pontosTuristicos2;
 
     // Entrada de dados para a primeira carta
     printf("Digite a letra do Estado (A-H) para a Carta 1 (ex: SP,RJ,MG): ");
-    scanf(" %c", &estado1);
+    scanf(" %2s", estado1);
     printf("Digite o código da Carta 1 (ex: A01): ");
-    scanf(" %s", codigo1);
+    scanf(" %3s", codigo1);
     printf("Digite o nome da Cidade da Carta 1 (sem espaços): ");
     scanf(" %s", nomeCidade1);
     printf("Digite a população da Cidade 1 (em milões): ");
-    scanf(" %d", &populacao1);
+    scanf(" %f", &populacao1);
     printf("Digite a área da Cidade 1 (em km²): ");
     scanf(" %f", &area1);
     printf("Digite o PIB da Cidade 1 (em bilhões): ");
@@ -30,13 +30,13 @@ int main() {
 
     // Entrada de dados para a segunda carta
     printf("\nDigite a letra do Estado (A-H) para a Carta 2 (ex: SP,RJ,MG): ");
-    scanf(" %c", &estado2);
+    scanf(" %2s", estado2);
     printf("Digite o código da Carta 2 (ex: B02): ");
-    scanf(" %s", codigo2);
+    scanf(" %3s", codigo2);
     printf("Digite o nome da Cidade da Carta 2 (sem espaços): ");
     scanf(" %s", nomeCidade2);
     printf("Digite a população da Cidade 2 (em milhões): ");
-    scanf(" %d", &populacao2);
+    scanf(" %f", &populacao2);
     printf("Digite a área da Cidade 2 (em km²): ");
     scanf(" %f", &area2);
     printf("Digite o PIB da Cidade 2 (em bilhões): ");
@@ -45,11 +45,9 @@ int main() {
     scanf(" %d", &pontosTuristicos2);
 
     // Exibição das cartas geradas com as informações
-    printf("\nCarta 1:\nEstado: %c\nCódigo: %s\nNome da Cidade: %s\nPopulação: %d milhões\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nNúmero de Pontos Turísticos: %d\n", estado1, codigo1, nomeCidade1, populacao1, area1, pib1, pontosTuristicos1);
+    printf("\nCarta 1:\nEstado: %2s\nCódigo: %3ss\nNome da Cidade: %s\nPopulação: %f milhões\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nNúmero de Pontos Turísticos: %d\n", estado1, codigo1, nomeCidade1, populacao1, area1, pib1, pontosTuristicos1);
 
-    printf("\nCarta 2:\nEstado: %c\nCódigo: %s\nNome da Cidade: %s\nPopulação: %d milhões\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nNúmero de Pontos Turísticos: %d\n", estado2, codigo2, nomeCidade2, populacao2, area2, pib2, pontosTuristicos2);
+    printf("\nCarta 2:\nEstado: %2s\nCódigo: %3s\nNome da Cidade: %s\nPopulação: %f milhões\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nNúmero de Pontos Turísticos: %d\n", estado2, codigo2, nomeCidade2, populacao2, area2, pib2, pontosTuristicos2);
 
     return 0;
 }
-
-
